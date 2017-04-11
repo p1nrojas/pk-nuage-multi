6a9bae94509fad866e9c8c39fb0b03d6557ac55d
p1nrojas <pinrojas@gmail.com>
2017-04-11 00:29:36 +0000

    defining ovs and sec_rules roles

--- install/install.sh
+++ install/install.sh
@@ -7,21 +7,14 @@ curl -fsSL https://get.docker.com/ | sh >> /tmp/install.log
 systemctl start docker >> /tmp/install.log
 systemctl enable docker >> /tmp/install.log
 
-#Install weave
-echo "$(date) Installing weave..."
-curl -fsSL git.io/weave -o /usr/local/bin/weave >> /tmp/install.log
-chmod a+x /usr/local/bin/weave >> /tmp/install.log
-weave launch --ipalloc-range 192.168.0.0/24 >> /tmp/install.log
-ip a add 192.168.0.1/24 dev weave >> /tmp/install.log
-
 #Create container images
 echo "$(date) Creating containers..."
-mkdir -p ~/github/packet-nuagevns >> /tmp/install.log
-cd ~/github/packet-nuagevns >> /tmp/install.log
-curl -fsS -o Dockerfile https://raw.githubusercontent.com/p1nrojas/packet-nuagevns/master/install/Dockerfile >> /tmp/install.log
-curl -fsS -o vimrc https://raw.githubusercontent.com/p1nrojas/packet-nuagevns/master/install/vimrc >> /tmp/install.log
-curl -fsS -o bash_profile https://raw.githubusercontent.com/p1nrojas/packet-nuagevns/master/install/bash_profile >> /tmp/install.log
-curl -fsS -o setup.sh https://raw.githubusercontent.com/p1nrojas/packet-nuagevns/master/install/setup.sh >> /tmp/install.log
+mkdir -p ~/github/packet-sdnlab >> /tmp/install.log
+cd ~/github/packet-sdnlab >> /tmp/install.log
+curl -fsS -o Dockerfile https://raw.githubusercontent.com/p1nrojas/packet-sdnlab/master/install/Dockerfile >> /tmp/install.log
+curl -fsS -o vimrc https://raw.githubusercontent.com/p1nrojas/packet-sdnlab/master/install/vimrc >> /tmp/install.log
+curl -fsS -o bash_profile https://raw.githubusercontent.com/p1nrojas/packet-sdnlab/master/install/bash_profile >> /tmp/install.log
+curl -fsS -o setup.sh https://raw.githubusercontent.com/p1nrojas/packet-sdnlab/master/install/setup.sh >> /tmp/install.log
 
 #Asking packet.net token
 echo ">>"
@@ -45,15 +38,16 @@ if [[ $nuage_license_key =~ \ + ]]; then echo "Format isn't right, bye!"; else e
 
 echo "$(date) Passed. Creating container image..."
 
-docker build -t p1nrojas/packet-nuagevns . >> /tmp/install.log
+docker build -t p1nrojas/packet-sdnlab . >> /tmp/install.log
 
 #Create data-only container
 echo "$(date) Creating data-only container..."
-docker run -d --name vns-data-only p1nrojas/packet-nuagevns true
+docker run -d --name vns-data-only p1nrojas/packet-sdnlab true
 
 echo "$(date) loading app files..."
-docker run -d -i -t --volumes-from vns-data-only --name vns-packet p1nrojas/packet-nuagevns
+docker run -d -i -t --volumes-from vns-data-only --name vns-packet p1nrojas/packet-sdnlab
 
+exit 0
 sleep 30
 
 echo "$(date) Copying public key to ansible..."
@@ -61,15 +55,15 @@ docker cp vns-data-only:/home/dev/.ssh/id_rsa.pub .
 cat id_rsa.pub >> ~/.ssh/authorized_keys ; rm -f id_rsa.pub
 
 echo "$(date) Creating bare_metal server and preparing weave..."
-docker run --rm --volumes-from vns-data-only p1nrojas/packet-nuagevns /home/dev/packet-nuagevns/packet.sh
+docker run --rm --volumes-from vns-data-only p1nrojas/packet-sdnlab /home/dev/packet-sdnlab/packet.sh
 
 echo "$(date) Installing Nuage VNS..."
-docker run --rm --volumes-from vns-data-only --network weave --ip 192.168.0.101 p1nrojas/packet-nuagevns /home/dev/packet-nuagevns/nuage.sh
+docker run --rm --volumes-from vns-data-only --network weave --ip 192.168.0.101 p1nrojas/packet-sdnlab /home/dev/packet-sdnlab/nuage.sh
 
 echo
 echo
 echo "================================================"
 echo "Summary Results"
-docker run --rm --volumes-from vns-data-only p1nrojas/packet-nuagevns cat /home/dev/packet-nuagevns/.summary
+docker run --rm --volumes-from vns-data-only p1nrojas/packet-sdnlab cat /home/dev/packet-sdnlab/.summary
 
 echo "$(date) done!"
--- install/setup.sh
+++ install/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ ! -f /var/log/ansible/ansible-packet-nuagevns.log ]; then
+if [ ! -f /var/log/ansible/ansible-packet-sdnlab.log ]; then
     echo "There is not data. Proceed setup"
-    git clone https://github.com/p1nrojas/packet-nuagevns ~/packet-nuagevns
-    touch /var/log/ansible/ansible-packet-nuagevns.log
+    git clone https://github.com/p1nrojas/packet-sdnlab ~/packet-sdnlab
+    touch /var/log/ansible/ansible-packet-sdnlab.log
     ssh-keygen -t rsa -b 4096 -C "dev@nuage.io" -f ~/.ssh/id_rsa -q -N ""
-    cd /home/dev/packet-nuagevns
+    cd /home/dev/packet-sdnlab
     ansible-playbook build.yml
     /bin/bash
 else
