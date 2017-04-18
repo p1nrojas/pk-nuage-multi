3d4a89f0c033bea84863f2260582e34bb3bc185a
Mauricio Rojas <pinrojas@gmail.com>
2017-04-18 00:34:07 +0000

    important issues solved. still in progess

--- roles/vsc-profiles/files/nuage_tls_profile.py
+++ roles/vsc-profiles/files/nuage_tls_profile.py
@@ -6,13 +6,14 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('vsc_host', type=str)
+parser.add_argument('ssh_port', type=str)
 parser.add_argument('vsc_ip', type=str)
 parser.add_argument('passwd', type=str)
 parser.add_argument('cert_name', type=str)
 args =  parser.parse_args()
 
 try:
-  child = pexpect.spawn('ssh admin@%s' % args.vsc_ip)
+  child = pexpect.spawn('ssh admin@%s -p %s' % (args.vsc_ip, args.ssh_port))
 #  child.logfile = sys.stdout  # uncomment to debug
   child.expect ('password:')
   child.sendline (args.passwd)
