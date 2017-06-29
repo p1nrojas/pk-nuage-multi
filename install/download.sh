24438afc08db76f9b89839213092ea76d3672fdd
Mauricio Rojas <pinrojas@gmail.com>
2017-06-29 12:10:42 -0500

    Update download.sh

--- install/download.sh
+++ install/download.sh
@@ -1,9 +1,9 @@
 #Create container images
 echo "$(date) Downloading files..."
-curl -fsS -o Dockerfile https://raw.githubusercontent.com/p1nrojas/pk-sdnlab-stdalone/master/install/Dockerfile 
-curl -fsS -o vimrc https://raw.githubusercontent.com/p1nrojas/pk-sdnlab-stdalone/master/install/vimrc 
-curl -fsS -o bash_profile https://raw.githubusercontent.com/p1nrojas/pk-sdnlab-stdalone/master/install/bash_profile 
-curl -fsS -o setup.sh https://raw.githubusercontent.com/p1nrojas/pk-sdnlab-stdalone/master/install/setup.sh 
+curl -fsS -o Dockerfile https://raw.githubusercontent.com/p1nrojas/pk-nuage-multi/master/install/Dockerfile 
+curl -fsS -o vimrc https://raw.githubusercontent.com/p1nrojas/pk-nuage-multi/master/install/vimrc 
+curl -fsS -o bash_profile https://raw.githubusercontent.com/p1nrojas/pk-nuage-multi/master/install/bash_profile 
+curl -fsS -o setup.sh https://raw.githubusercontent.com/p1nrojas/pk-nuage-multi/master/install/setup.sh 
 
 #Asking packet.net token
 echo ">>"
