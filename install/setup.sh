c275a9adb14aad5922d48afadadb014751e9e3fc
Mauricio Rojas <pinrojas@gmail.com>
2017-06-29 12:12:17 -0500

    Update setup.sh

--- install/setup.sh
+++ install/setup.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-if [ ! -f /var/log/ansible/ansible-pk-sdnlab-stdalone.log ]; then
+if [ ! -f /var/log/ansible/ansible-pk-nuage-multi.log ]; then
     echo "There is not data. Proceed setup"
-    git clone https://github.com/p1nrojas/pk-sdnlab-stdalone ~/pk-sdnlab-stdalone
-    touch /var/log/ansible/ansible-pk-sdnlab-stdalone.log
+    git clone https://github.com/p1nrojas/pk-nuage-multi ~/pk-nuage-multi
+    touch /var/log/ansible/ansible-pk-nuage-multi.log
     ssh-keygen -t rsa -b 4096 -C "dev@nuage.io" -f ~/.ssh/id_rsa -q -N ""
-    cd /home/dev/pk-sdnlab-stdalone
-    ansible-playbook build.yml
     /bin/bash
 else
     echo "Caution: user data detected. skipping setup. Next time override CMD"
